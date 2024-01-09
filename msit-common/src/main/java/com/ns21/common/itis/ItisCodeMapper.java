7f0ecab54ac58f548edaec8f3a9d2465c04ebb13
kjg0898 <kjg0898@gmail.com>
2024-01-09 17:04:08 +0900

    2023-01-09 김진건 내용 : itis code 매핑 추가

--- msit-common/src/main/java/com/ns21/common/itis/ItisCodeMapper.java
+++ msit-common/src/main/java/com/ns21/common/itis/ItisCodeMapper.java
@@ -17,29 +17,36 @@ public class ItisCodeMapper {
         // categoryName을 기반으로 ITIS 코드를 반환합니다.
         // 해당 카테고리에 유효한 ITIS 코드가 없으면 null을 반환합니다.
         switch (categoryName) {
-            case "dynamic_object.vehicle.truck":
-                return ItisCodes.HAZARDOUS_MATERIAL_VEHICLE;
+            case "dynamic_object.vehicle.police_car":
+                return ItisCodes.POLICE_CAR;
+            case "dynamic_object.vehicle.ambulance":
+                return ItisCodes.AMBULANCE;
+            case "dynamic_object.vehicle.fire_truck":
+                return ItisCodes.FIRE_TRUCK;
             case "dynamic_object.human.pedestrian":
                 return ItisCodes.PEDESTRIAN_ON_ROAD;
-            case "movable_object.barrier":
-                return ItisCodes.ROAD_CLOSURE_LANE_BLOCKAGE;
+            case "dynamic_object.human.police_officer":
+                return ItisCodes.POLICE_CAR;
+            case "dynamic_object.human.construction_worker":
+                return ItisCodes.PEDESTRIAN_ON_ROAD;
+            case "dynamic_object.human.firefighter":
+                return ItisCodes.FIRE_TRUCK;
+            case "dynamic_object.human.stroller":
+                return ItisCodes.PEDESTRIAN_ON_ROAD;
+            case "dynamic_object.human.wheelchair":
+                return ItisCodes.PEDESTRIAN_ON_ROAD;
+
             case "movable_object.traffic_cone":
                 return ItisCodes.MOBILE_CONSTRUCTION;
+            case "movable_object.barrier":
+                return ItisCodes.ROAD_CLOSURE_LANE_BLOCKAGE;
+            case "movable_object.debris":
+                return ItisCodes.FALLING_OBJECTS;
+            case "movable_object.other_movable_object":
+                return ItisCodes.FALLING_OBJECTS;
             default:
                 return null;
 
-        }
-    }
-    //vehicleState 로 ITIS 코드 결정
-    public static Integer getVehicleStateITISCode(String vehicleState) {
-        // 해당 차량 상태에 유효한 ITIS 코드가 없으면 null을 반환합니다.
-        switch (vehicleState) {
-            case "moving":
-                return ItisCodes.STOP_AND_GO_TRAFFIC;
-            case "stopped":
-                return ItisCodes.STOPPED_VEHICLE;
-            default:
-                return null; // 이 차량 상태에 유효한 ITIS 코드가 없음을 나타냅니다.
         }
     }
 }
