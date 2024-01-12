7607fba6bf08e43728759cc7a64ddc8ef5faf5dc
kjg0898 <kjg0898@gmail.com>
2024-01-12 14:45:32 +0900

    2024-01-10 김진건 내용 : switch 식으로 변경

--- msit-common/src/main/java/com/ns21/common/itis/ItisCodeMapper.java
+++ msit-common/src/main/java/com/ns21/common/itis/ItisCodeMapper.java
@@ -16,37 +16,21 @@ public class ItisCodeMapper {
     public static Integer getCategoryITISCode(String categoryName) {
         // categoryName을 기반으로 ITIS 코드를 반환합니다.
         // 해당 카테고리에 유효한 ITIS 코드가 없으면 null을 반환합니다.
-        switch (categoryName) {
-            case "dynamic_object.vehicle.police_car":
-                return ItisCodes.POLICE_CAR;
-            case "dynamic_object.vehicle.ambulance":
-                return ItisCodes.AMBULANCE;
-            case "dynamic_object.vehicle.fire_truck":
-                return ItisCodes.FIRE_TRUCK;
-            case "dynamic_object.human.pedestrian":
-                return ItisCodes.PEDESTRIAN_ON_ROAD;
-            case "dynamic_object.human.police_officer":
-                return ItisCodes.POLICE_CAR;
-            case "dynamic_object.human.construction_worker":
-                return ItisCodes.PEDESTRIAN_ON_ROAD;
-            case "dynamic_object.human.firefighter":
-                return ItisCodes.FIRE_TRUCK;
-            case "dynamic_object.human.stroller":
-                return ItisCodes.PEDESTRIAN_ON_ROAD;
-            case "dynamic_object.human.wheelchair":
-                return ItisCodes.PEDESTRIAN_ON_ROAD;
-
-            case "movable_object.traffic_cone":
-                return ItisCodes.MOBILE_CONSTRUCTION;
-            case "movable_object.barrier":
-                return ItisCodes.ROAD_CLOSURE_LANE_BLOCKAGE;
-            case "movable_object.debris":
-                return ItisCodes.FALLING_OBJECTS;
-            case "movable_object.other_movable_object":
-                return ItisCodes.FALLING_OBJECTS;
-            default:
-                return null;
-
-        }
+        return switch (categoryName) {
+            case "dynamic_object.vehicle.police_car" -> ItisCodes.POLICE_CAR;
+            case "dynamic_object.vehicle.ambulance" -> ItisCodes.AMBULANCE;
+            case "dynamic_object.vehicle.fire_truck" -> ItisCodes.FIRE_TRUCK;
+            case "dynamic_object.human.pedestrian" -> ItisCodes.PEDESTRIAN_ON_ROAD;
+            case "dynamic_object.human.police_officer" -> ItisCodes.POLICE_CAR;
+            case "dynamic_object.human.construction_worker" -> ItisCodes.PEDESTRIAN_ON_ROAD;
+            case "dynamic_object.human.firefighter" -> ItisCodes.FIRE_TRUCK;
+            case "dynamic_object.human.stroller" -> ItisCodes.PEDESTRIAN_ON_ROAD;
+            case "dynamic_object.human.wheelchair" -> ItisCodes.PEDESTRIAN_ON_ROAD;
+            case "movable_object.traffic_cone" -> ItisCodes.MOBILE_CONSTRUCTION;
+            case "movable_object.barrier" -> ItisCodes.ROAD_CLOSURE_LANE_BLOCKAGE;
+            case "movable_object.debris" -> ItisCodes.FALLING_OBJECTS;
+            case "movable_object.other_movable_object" -> ItisCodes.FALLING_OBJECTS;
+            default -> null;
+        };
     }
 }
